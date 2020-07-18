0df22280779332a88d2a7e71ff64cb33940db2d0
monpro <monproshen@gmail.com>
2020-07-18 17:41:09 +1000

    init monpro_book project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <span class="icon-bookmark"></span>
+    <span class="text"> ABCD</span>
   </div>
 </template>
+<script>
+  export default {}
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > 50 ? 50 : fontSize
+    html.style.fontSize = fontSize + 'px'
+    console.log('content loader', html.style)
+  })
+</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style lang="scss" scoped>
+  @import "./assets/styles/global.scss";
+  .text {
+    font-family: 'Days One';
+    font-size: pxToRem(20);
+    color: orange;
+  }
 </style>
