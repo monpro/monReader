db3949b58759bccd3d9dd25835a49bd18c5c169c
monpro <monproshen@gmail.com>
2020-07-19 16:18:44 +1000

    implement prevPage and nextPage gesture func

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,6 @@
     let fontSize = window.innerWidth / 10
     fontSize = fontSize > 50 ? 50 : fontSize
     html.style.fontSize = fontSize + 'px'
-    console.log('content loader', html.style)
   })
 </script>
 
--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -13,17 +13,44 @@
       ...mapGetters(['fileName'])
     },
     methods: {
+      prevPage() {
+        if (this.rendition) {
+          this.rendition.prev()
+        }
+      },
+      nextPage() {
+        if (this.rendition) {
+          this.rendition.next(0)
+        }
+      },
+      showTitleAndMenu() {
+        return null
+      },
       initEpub() {
         const baseUrl = 'http://localhost:8081/epub/'
         const url = baseUrl + this.fileName + '.epub'
-        console.log(url)
         this.book = new Epub(url)
-        console.log(this.book)
         this.rendition = this.book.renderTo('read', {
           width: innerWidth,
           height: innerHeight
         })
         this.rendition.display()
+        this.rendition.on('touchstart', event => {
+          this.touchStartX = event.changedTouches[0].clientX
+          this.touchStartTime = event.timeStamp
+        })
+        this.rendition.on('touchend', event => {
+          const offsetX = event.changedTouches[0].clientX - this.touchStartX
+          const time = event.timeStamp - this.touchStartTime
+          if (time < 500 && offsetX > 40) {
+            this.prevPage()
+          } else if (time < 500 && offsetX < -40) {
+            this.nextPage()
+          } else {
+            this.showTitleAndMenu()
+          }
+          console.log(offsetX, time)
+        })
       }
     },
     mounted() {
