a988b14c777992a51dfe9134a61cde984648e543
monpro <monproshen@gmail.com>
2020-07-20 23:16:22 +1000

    add multiple state management variables

--- src/store/module/book.js
+++ src/store/module/book.js
@@ -4,7 +4,22 @@ const book = {
     menuVisible: false,
     // -1 not visible, 0 show font size, 1 show theme 2 show progress 3 show popup
     settingVisible: false,
-    defaultFontSize: 16
+    defaultFontSize: 16,
+    defaultFontFamily: 'Default',
+    fontFamilyVisible: false,
+    defaultTheme: 'Default',
+    bookAvailable: false,
+    progress: 0,
+    section: 0,
+    isPaginating: true,
+    currentBook: null,
+    navigation: null,
+    cover: null,
+    metadata: null,
+    paginate: '',
+    pagelist: null,
+    offsetY: 0,
+    isBookmark: null
   },
   mutations: {
     SET_FILENAME: (state, fileName) => {
@@ -18,6 +33,48 @@ const book = {
     },
     SET_DEFAULT_FONT_SIZE: (state, fontSize) => {
       state.defaultFontSize = fontSize
+    },
+    SET_FONT_FAMILY_VISIBLE: (state, visible) => {
+      state.fontFamilyVisible = visible
+    },
+    SET_DEFAULT_THEME: (state, theme) => {
+      state.defaultTheme = theme
+    },
+    SET_BOOK_AVAILABLE: (state, bookAvailable) => {
+      state.bookAvailable = bookAvailable
+    },
+    SET_PROGRESS: (state, progress) => {
+      state.progress = progress
+    },
+    SET_SECTION: (state, section) => {
+      state.section = section
+    },
+    SET_IS_PAGINATING: (state, isPaginating) => {
+      state.isPaginating = isPaginating
+    },
+    SET_CURRENT_BOOK: (state, currentBook) => {
+      state.currentBook = currentBook
+    },
+    SET_NAVIGATION: (state, navigation) => {
+      state.navigation = navigation
+    },
+    SET_COVER: (state, cover) => {
+      state.cover = cover
+    },
+    SET_METADATA: (state, metadata) => {
+      state.metadata = metadata
+    },
+    SET_PAGINATE: (state, paginate) => {
+      state.paginate = paginate
+    },
+    SET_PAGELIST: (state, pagelist) => {
+      state.pagelist = pagelist
+    },
+    SET_OFFSETY: (state, offsetY) => {
+      state.offsetY = offsetY
+    },
+    SET_IS_BOOKMARK: (state, isBookmark) => {
+      state.isBookmark = isBookmark
     }
   }
 }
