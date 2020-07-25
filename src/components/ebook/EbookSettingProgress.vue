747758b951fc1a46b51952b0ae30ac591bf14cde
monpro <monproshen@gmail.com>
2020-07-26 09:43:02 +1000

    add Setting Progress Component

---
+++ src/components/ebook/EbookSettingProgress.vue
@@ -0,0 +1,86 @@
+<template>
+  <transition name="slide-up">
+    <div class="setting-wrapper" v-show="menuVisible && settingVisible === 2">
+      <div class="setting-progress">
+        <div class="progress-wrapper">
+          <input class="progress" type="range"
+                 max="100"
+                 min="0"
+                 step="1"
+                 @change="onProgressChange($event.target.value)" @input="onProgressInput($event.target.value)"
+                 :value="progress"
+                 :disabled="!bookAvailable"
+                 ref="progress">
+        </div>
+        <div class="text-wrapper">
+          <span>{{bookAvailable ? progress + '%' : 'Loading...'}}</span>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  import { ebookMixin } from '../../utils/mixin'
+  export default {
+    mixins: [ebookMixin],
+    methods: {
+      onProgressChange(progress) {},
+      onProgressInput(progress) {}
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+  .setting-wrapper {
+    position: absolute;
+    bottom: pxToRem(48);
+    left: 0;
+    z-index: 101;
+    width: 100%;
+    height: pxToRem(60);
+    background: white;
+    box-shadow: 0 pxToRem(-8) pxToRem(8) rgba(0, 0, 0, .15);
+    .setting-progress {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      .progress-wrapper {
+        width: 100%;
+        height: 100%;
+        @include center;
+        padding: 0 pxToRem(30);
+        box-sizing: border-box;
+        .progress {
+          width: 100%;
+          -webkit-appearance: none;
+          height: pxToRem(2);
+          background: -webkit-linear-gradient(#999, #999) no-repeat, #ddd;
+          background-size: 0 100%;
+          &:focus {
+            outline: none;
+          }
+          &::-webkit-slider-thumb {
+            -webkit-appearance: none;
+            height: pxToRem(20);
+            width: pxToRem(20);
+            border-radius: 50%;
+            background: white;
+            box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .15);
+            border: pxToRem(1) solid #ddd;
+          }
+        }
+      }
+      .text-wrapper {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        color: #333;
+        font-size: pxToRem(12);
+        text-align: center;
+      }
+    }
+  }
+</style>
