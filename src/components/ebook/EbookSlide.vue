c68fcc33a9c149dd5a93deaf90ef3ba54d2aa125
monpro <monproshen@gmail.com>
2020-07-27 10:59:54 +1000

    add EbookSlide component

---
+++ src/components/ebook/EbookSlide.vue
@@ -0,0 +1,58 @@
+<template>
+  <transition name="fade">
+    <div class="slide-content-wrapper" v-show="menuVisible && settingVisible === 3">
+      <transition name="slide-right">
+        <div class="content">
+          <div class="content-page-wrapper">
+            <div class="content-page"></div>
+            <div class="content-page-tab"></div>
+          </div>
+        </div>
+      </transition>
+      <div class="content-bg" @click="hideTitleAndMenu"></div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  import { ebookMixin } from '../../utils/mixin'
+  export default {
+    mixins: [ebookMixin],
+    methods: {
+      hide() {
+
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  @import "../../assets/styles/global";
+  .slide-content-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 300;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    .content {
+      flex: 0 0 85%;
+      width: 85%;
+      height: 100%;
+      .content-page-wrapper {
+        .content-page {
+
+        }
+        .content-page-tab {
+
+        }
+      }
+    }
+    .content-bg {
+      flex: 0 0 15%;
+      width: 15%;
+      height: 100%;
+    }
+  }
+</style>
