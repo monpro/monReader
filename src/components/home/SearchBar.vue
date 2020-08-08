dd4bbd5499c4aaa97204a083e627d1d9e7e8dc50
monpro <monproshen@gmail.com>
2020-08-08 16:45:46 +1000

    finish the layout of search input

--- src/components/home/SearchBar.vue
+++ src/components/home/SearchBar.vue
@@ -11,13 +11,25 @@
         <span class="icon-shake icon"></span>
       </div>
     </div>
-    <div class="search-bar-input-wrapper"></div>
+    <div class="search-bar-input-wrapper">
+      <div class="search-bar-input">
+        <span class="icon-search icon"></span>
+        <input type="text"
+                 class="input"
+                 :placeholder="$t('home.hint')"
+                 v-model="searchText">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'SearchBar'
+    data() {
+      return {
+        searchText: ''
+      }
+    }
   }
 </script>
 
@@ -27,7 +39,7 @@
     position: relative;
     width: 100%;
     .search-bar-title-wrapper {
-      position: absolute;
+      position: relative;
       top: 0;
       left: 0;
       width: 100%;
@@ -59,6 +71,38 @@
         }
       }
     }
-    .search-bar-input-wrapper {}
+    .search-bar-input-wrapper {
+      width: 100%;
+      height: pxToRem(52);
+      padding: pxToRem(10);
+      box-sizing: border-box;
+      .search-bar-input {
+        width: 100%;
+        background: #f4f4f4;
+        border-radius: pxToRem(20);
+        padding: pxToRem(5) pxToRem(15);
+        box-sizing: border-box;
+        border: pxToRem(1) solid #eee;
+        @include left;
+        .icon-search {
+          color: #999;
+        }
+        .input {
+          width: 100%;
+          height: pxToRem(22);
+          border: none;
+          background: transparent;
+          margin-left: pxToRem(10);
+          font-size: pxToRem(12);
+          color: #666;
+          &:focus {
+            outline: none;
+          }
+          &::-webkit-input-placeholder {
+            color: #ccc;
+          }
+        }
+      }
+    }
   }
 </style>
