f8032bc4d45d737a266ff3118e49d9c75909b16d
monpro <monproshen@gmail.com>
2020-08-08 16:08:57 +1000

    set up basic router of Library Page

---
+++ src/views/library/LibraryHome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="library-home">
+    123
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LibraryHome'
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+  .library-home {
+    width: 100%;
+    height: 100%;
+    background: white;
+  }
+</style>
---
+++ src/views/library/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="library">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index'
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+  .library {
+    width: 100%;
+    height: 100%;
+    background: white;
+  }
+</style>
