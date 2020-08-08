cf58ba00ba7248099dc2d19dd52bb425abaf329b
monpro <monproshen@gmail.com>
2020-08-08 21:26:11 +1000

    add interactive search input when scroll down and up

--- src/components/home/SearchBar.vue
+++ src/components/home/SearchBar.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="search-bar">
-    <div class="search-bar-title-wrapper">
-      <div class="title-icon-back-wrapper">
-        <span class="icon-back icon"></span>
+  <div class="search-bar" :class="{'hide-title': !titleVisible}">
+    <transition name="title-move">
+      <div class="search-bar-title-wrapper" v-show="titleVisible">
+
+        <div class="title-text-wrapper">
+          <span class="title-text title">{{$t('home.title')}}</span>
+        </div>
+        <div class="title-icon-shake-wrapper">
+          <span class="icon-shake icon"></span>
+        </div>
       </div>
-      <div class="title-text-wrapper">
-        <span class="title-text title">{{$t('home.title')}}</span>
-      </div>
-      <div class="title-icon-shake-wrapper">
-        <span class="icon-shake icon"></span>
-      </div>
+    </transition>
+    <div class="title-icon-back-wrapper">
+      <span class="icon-back icon"></span>
     </div>
-    <div class="search-bar-input-wrapper">
+    <div class="search-bar-input-wrapper" :class="{'hide-title': !titleVisible}">
       <div class="search-bar-input">
         <span class="icon-search icon"></span>
         <input type="text"
-                 class="input"
-                 :placeholder="$t('home.hint')"
-                 v-model="searchText">
+               class="input"
+               :placeholder="$t('home.hint')"
+               v-model="searchText">
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import { libraryMixin } from '../../utils/mixin'
+
   export default {
+    mixins: [libraryMixin],
     data() {
       return {
-        searchText: ''
+        searchText: '',
+        titleVisible: true
+      }
+    },
+    watch: {
+      offsetY(offsetY) {
+        if (offsetY > 0) {
+          this.hideTitle()
+        } else {
+          this.showTitle()
+        }
+      }
+    },
+    methods: {
+      hideTitle() {
+        this.titleVisible = false
+      },
+      showTitle() {
+        this.titleVisible = true
       }
     }
   }
@@ -38,20 +62,17 @@
   .search-bar {
     position: relative;
     width: 100%;
+    height: pxToRem(94);
+    z-index: 150;
+    &.hide-title {
+      height: pxToRem(52);
+    }
     .search-bar-title-wrapper {
-      position: relative;
+      position: absolute;
       top: 0;
       left: 0;
       width: 100%;
       height: pxToRem(42);
-      .title-icon-back-wrapper {
-        position: absolute;
-        left: pxToRem(15);
-        top: 0;
-        height: pxToRem(42);
-        @include center;
-        .icon-back {}
-      }
       .title-text-wrapper {
         width: 100%;
         height: pxToRem(42);
@@ -71,11 +92,26 @@
         }
       }
     }
+    .title-icon-back-wrapper {
+      position: absolute;
+      left: pxToRem(15);
+      top: 0;
+      height: pxToRem(42);
+      @include center;
+      .icon-back {}
+    }
     .search-bar-input-wrapper {
+      position: absolute;
+      left: 0;
+      top: pxToRem(42);
       width: 100%;
       height: pxToRem(52);
       padding: pxToRem(10);
       box-sizing: border-box;
+      transition: top .2s linear;
+      &.hide-title {
+        top: 0;
+      }
       .search-bar-input {
         width: 100%;
         background: #f4f4f4;
--- src/views/library/LibraryHome.vue
+++ src/views/library/LibraryHome.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="library-home">
     <search-bar></search-bar>
+    <scroll :top="94" @onScroll="onScroll">
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+      <div>11111111</div>
+    </scroll>
   </div>
 </template>
 
 <script>
   import SearchBar from '../../components/home/SearchBar'
+  import Scroll from '../../components/common/Scroll'
+  import { libraryMixin } from '../../utils/mixin'
 
   export default {
+    mixins: [libraryMixin],
     components: {
-      SearchBar
+      SearchBar,
+      Scroll
+    },
+    methods: {
+      onScroll(offsetY) {
+        this.setOffsetY(offsetY)
+      }
     }
   }
 </script>
