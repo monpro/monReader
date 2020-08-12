c672632e9847cc3ad3ac72664cb3cd995ffd2f19
monpro <monproshen@gmail.com>
2020-08-12 23:07:13 +1000

    finish the layout of flapCard

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flap-card-wrapper" v-show="flapCardVisible">
     <div class="flap-card-bg">
-      <div class="flap-card" v-for="(item, index) in flapCardList" :key="index">
+      <div class="flap-card" v-for="(item, index) in flapCardList" :key="index" :style="{zIndex: item.zIndex}">
         <div class="flap-card-circle">
-          <div class="flap-card-semi-circle flap-card-semi-circle-left"></div>
-          <div class="flap-card-semi-circle flap-card-semi-circle-right"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCircleStyle(item, 'left')"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-right" :style="semiCircleStyle(item, 'right')"></div>
         </div>
       </div>
     </div>
@@ -22,6 +22,13 @@
     methods: {
       close() {
         this.setFlapCardVisible(false)
+      },
+      semiCircleStyle(item, direction) {
+        return {
+          backgroundColor: `rgba(${item.r}, ${item.g}, ${item.b})`,
+          backgroundSize: item.backgroundSize,
+          backgroundImage: direction === 'left' ? item.imgLeft : item.imgRight
+        }
       }
     },
     data() {
@@ -91,15 +98,44 @@
 <style lang="scss" rel="stylesheet/scss" scoped>
   @import "../../assets/styles/global";
   .flap-card-wrapper {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+    @include absCenter;
     width: 100%;
     height: 100%;
     z-index: 1000;
     background: rgba(0, 0, 0, .6);
+    @include center;
+    .flap-card-bg {
+      position: relative;
+      width: pxToRem(64);
+      height: pxToRem(64);
+      border-radius: pxToRem(5);
+      background: white;
+      .flap-card {
+        width: pxToRem(48);
+        height: pxToRem(48);
+        @include absCenter;
+        margin: auto;
+        &-circle {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          .flap-card-semi-circle {
+            flex: 0 0 50%;
+            width: 50%;
+            height: 100%;
+            background-repeat: no-repeat;
+            &-left {
+              border-radius: pxToRem(24) 0 0 pxToRem(24);
+              background-position: center right;
+            }
+            &-right {
+              border-radius: 0 pxToRem(24) pxToRem(24) 0;
+              background-position: center left;
+            }
+          }
+        }
+      }
+    }
     .close-btn-wrapper {
       position: absolute;
       left: 0;
