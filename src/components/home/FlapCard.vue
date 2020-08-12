b5d5e8f08759e9cc3707161361d15a1ac0e7889b
monpro <monproshen@gmail.com>
2020-08-12 22:44:04 +1000

    add flapCardList function and using imgs

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="flap-card-wrapper" v-show="flapCardVisible">
+    <div class="flap-card-bg">
+      <div class="flap-card" v-for="(item, index) in flapCardList" :key="index">
+        <div class="flap-card-circle">
+          <div class="flap-card-semi-circle flap-card-semi-circle-left"></div>
+          <div class="flap-card-semi-circle flap-card-semi-circle-right"></div>
+        </div>
+      </div>
+    </div>
     <div class="close-btn-wrapper" @click="close">
       <span class="icon-close"></span>
     </div>
@@ -14,6 +22,67 @@
     methods: {
       close() {
         this.setFlapCardVisible(false)
+      }
+    },
+    data() {
+      return {
+        flapCardList: [
+          {
+            r: 255,
+            g: 102,
+            _g: 102,
+            b: 159,
+            imgLeft: 'url(' + require('@/assets/images/gift-left.png') + ')',
+            imgRight: 'url(' + require('@/assets/images/gift-right.png') + ')',
+            backgroundSize: '50% 50%',
+            zIndex: 100,
+            rotateDegree: 0
+          },
+          {
+            r: 74,
+            g: 171,
+            _g: 171,
+            b: 255,
+            imgLeft: 'url(' + require('@/assets/images/compass-left.png') + ')',
+            imgRight: 'url(' + require('@/assets/images/compass-right.png') + ')',
+            backgroundSize: '50% 50%',
+            zIndex: 99,
+            rotateDegree: 0
+          },
+          {
+            r: 255,
+            g: 198,
+            _g: 198,
+            b: 102,
+            imgLeft: 'url(' + require('@/assets/images/star-left.png') + ')',
+            imgRight: 'url(' + require('@/assets/images/star-right.png') + ')',
+            backgroundSize: '50% 50%',
+            zIndex: 98,
+            rotateDegree: 0
+          },
+          {
+            r: 255,
+            g: 102,
+            _g: 102,
+            b: 159,
+            imgLeft: 'url(' + require('@/assets/images/heart-left.png') + ')',
+            imgRight: 'url(' + require('@/assets/images/heart-right.png') + ')',
+            backgroundSize: '50% 50%',
+            zIndex: 97,
+            rotateDegree: 0
+          },
+          {
+            r: 59,
+            g: 201,
+            _g: 201,
+            b: 22,
+            imgLeft: 'url(' + require('@/assets/images/crown-left.png') + ')',
+            imgRight: 'url(' + require('@/assets/images/crown-right.png') + ')',
+            backgroundSize: '50% 50%',
+            zIndex: 96,
+            rotateDegree: 0
+          }
+        ]
       }
     }
   }
