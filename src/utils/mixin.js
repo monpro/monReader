1d0a4aed93662de6df141c8ef143c9c338289894
monpro <monproshen@gmail.com>
2020-08-22 13:17:33 +1000

    add helper functions about display detail

--- src/utils/mixin.js
+++ src/utils/mixin.js
@@ -141,6 +141,15 @@ export const libraryMixin = {
     ...mapGetters(['offsetY', 'hotSearchOffsetY', 'flapCardVisible'])
   },
   methods: {
-    ...mapActions(['setOffsetY', 'setHotSearchOffsetY', 'setFlapCardVisible'])
+    ...mapActions(['setOffsetY', 'setHotSearchOffsetY', 'setFlapCardVisible']),
+    showBookDetail(book) {
+      this.$router.push({
+        path: '/library/detail',
+        query: {
+          fileName: book.fileName,
+          category: book.categoryText
+        }
+      })
+    }
   }
 }
