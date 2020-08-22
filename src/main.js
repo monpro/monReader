c42964c94c5b74ed1bfb8912850a4a461de778bc
monpro <monproshen@gmail.com>
2020-08-22 13:18:17 +1000

    move api interface into config.devServer

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import i18n from './lang'
 import './assets/styles/icon.css'
 import './assets/fonts/daysOne.css'
-import './mock'
+// import './mock'
 Vue.config.productionTip = false
 
 new Vue({
