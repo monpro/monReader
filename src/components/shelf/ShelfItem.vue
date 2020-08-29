0a5bde780da3de9393da86280bc77dadece0a2e9
monpro <monproshen@gmail.com>
2020-08-29 15:21:19 +1000

    add dynamic calculated layout of ShelfItem

--- src/components/shelf/ShelfItem.vue
+++ src/components/shelf/ShelfItem.vue
@@ -38,4 +38,9 @@
 
 <style lang="scss" rel="stylesheet/scss" scoped>
   @import "../../assets/styles/global";
+  .shelf-item {
+    width: 100%;
+    height: 100%;
+    background-color: red;
+  }
 </style>
--- src/components/shelf/ShelfList.vue
+++ src/components/shelf/ShelfList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shelf-list">
-    <div v-for="item in shelfList" :key="item.id">
-      <shelf-item :data="item"></shelf-item>
+    <div class="shelf-list-item-wrapper" v-for="item in shelfList" :key="item.id">
+      <shelf-item :data="item" :style="{height: itemHeight}"></shelf-item>
     </div>
   </div>
 </template>
@@ -9,10 +9,16 @@
 <script>
   import { shelfMixin } from '../../utils/mixin'
   import ShelfItem from './ShelfItem'
+  import { getCurPx } from '../../utils/utils'
 
   export default {
     components: { ShelfItem },
-    mixins: [shelfMixin]
+    mixins: [shelfMixin],
+    computed: {
+      itemHeight() {
+        return ((window.innerWidth - getCurPx(120)) / 3) / 250 * 350 + 'px'
+      }
+    }
   }
 </script>
 
@@ -23,5 +29,16 @@
     top: pxToRem(94);
     left: 0;
     z-index: 100;
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+    padding: 0 pxToRem(15);
+    box-sizing: border-box;
+    &-item-wrapper {
+      flex: 0 0 33.3%;
+      width: 33.3%;
+      padding: pxToRem(15);
+      box-sizing: border-box;
+    }
   }
 </style>
